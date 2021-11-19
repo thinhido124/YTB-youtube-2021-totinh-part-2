cf6eea5477cd8072dcde6780c00339279e54e094
thinhido124 <86718925+thinhido124@users.noreply.github.com>
2021-11-19 22:41:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
     introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
-    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    introDesc: `em có điều này muốn hỏi cj, cj nhớ phải trả lời thật lòng nha`,
     btnIntro: 'OK',
-    title: 'Anh biết Em thích Anh phải không phải không ? 😙',
-    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
-    btnYes: 'Có, em thật sự thích anh 💖💖💖',
-    btnNo: '💔 Anh mơ à ! 💔 ',
-    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
+    title: 'em biết cj thích em phải không phải không ? 😙',
+    desc: 'cj không trả lời hoặc thoát ra, tức là cj thích em đó nha :> ',
+    btnYes: 'Có, cj thật sự thích em 💖💖💖',
+    btnNo: '💔 em mơ à ! 💔 ',
+    question:'Nói cho em biết đi vì sao cj thích em 😊😊',
     reasonPlaceholder: 'Lý do',
     btnReply: 'Gởi ! 💌💌',
-    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
-    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
-    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    reply: 'Tại vì em dễ thw đó Ahiiiii 😊😊',
+    mess: 'em biết mà, em cũng thích cj lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho em  nha. 💬💬',
     btnAccept: 'Ok lun 💖',
-    messLink: 'https://m.me/PhungQuocDinh'
+    messLink: 'https://facebook.com/khai.dvfb'
 }
